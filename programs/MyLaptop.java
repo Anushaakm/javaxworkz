c3c0fffa0db541b81fc26341660fe2760ecd66fa
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-13 10:19:52 +0530

    my first push

---
+++ programs/MyLaptop.java
@@ -0,0 +1,38 @@
+class MyLaptop{
+	public static void main(String[] dell){
+		byte numofkeys=101;
+		byte systemtype=64;
+		short priceofkeyboard=645;
+		int priceoflaptop=60000;
+		long lnum=1234567890L;
+		float jversion=1.8f, ram=8.00f;
+		double dnum=19044.1889;
+		boolean ssd=true;
+		char ch='i';
+		
+		
+		
+		System.out.println("My laptop information");
+		
+		System.out.println("Number of keys in keyboard:"+numofkeys);
+		
+		System.out.println("Number keys in keyboard:"+lnum);
+		
+		System.out.println("Price of dell keyboard:"+priceofkeyboard);
+		
+		System.out.println("Price of dell laptop:"+priceoflaptop);
+		
+		System.out.println("Java version installed in loptop:"+jversion);
+		
+		System.out.println("processer type intel core:"+ch+"5");
+		
+		System.out.println("SSD storage available:"+ssd);
+		
+		System.out.println("OS eddition: Windows 10 and  OS build:"+dnum );
+		
+		System.out.println("Installed RAM: "+ram+"GB" );
+		System.out.println("System type:"+systemtype+"bit");
+		
+		
+	}
+}
