4b666d4694a36e98e1ef29b95536f05865551eb9
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-13 21:16:55 +0530

    parameter

---
+++ programs/Identity.java
@@ -0,0 +1,65 @@
+class Identity{
+	static void PrintNameAndEmail(String name,String email){
+		System.out.println("Enter PrintNameAndEmail ");
+		System.out.println("Name: "+name+ "email: "+email);
+		
+	}
+	static void PrintNameAndMobile(String name,long mbl){
+		System.out.println("Enter PrintNameAndMobile ");
+		System.out.println("Name: "+name+ "Ph no: "+mbl);
+		
+	}
+	static void PrintNameAndAge(String name,int age){
+		System.out.println("Enter PrintNameAndAge ");
+		System.out.println("Name: "+name+ "age: "+age);
+		
+	}
+	static void PrintNameAndAddress(String name,String address){
+		System.out.println("Enter PrintNameAndAddress ");
+		System.out.println("Name: "+name+ "address: "+address);
+		
+	}
+	 static void PrintNameAndEmailAndAddressAndAge(String name,String email,String address,int age){
+		System.out.println("Enter PrintNameAndEmailAndAddressAndAge ");
+		System.out.println("Name: "+name+"Email: "+email+"address: "+address+"Age: "+age);
+		
+	}
+	static void PrintNameAndGender(String name,String gender){
+		System.out.println("Enter PrintNameAndGender ");
+		System.out.println("Name: "+name+ "Gender: "+gender);
+		
+	}
+	static void PrintNameAndWorking(String name,boolean working){
+		System.out.println("Enter PrintNameAndWorking ");
+		System.out.println("Name: "+name+ "Working: "+working);
+		
+	}
+	 static void PrintNameAndShirtSize(String name,int shirtSize){
+		System.out.println("Enter PrintNameAndShirtSize ");
+		System.out.println("Name: "+name+ "Shirt Size: "+shirtSize);
+		
+	}
+	 static void PrintNameAndPantSize(String name,int pantSize){
+		System.out.println("Enter PrintNameAndPantSize ");
+		System.out.println("Name: "+name+ "Pant Size: "+pantSize);
+		
+	}
+	static void PrintNameAndWeightAndHeight(String name,int weight,int height){
+		System.out.println("Enter PrintNameAndWeightAndHeight ");
+		System.out.println("Name: "+name+ "Weight: "+weight+"Height: "+height);
+		
+	}
+	 
+	
+	
+	
+	
+	
+} 
+
+
+
+
+
+
+
---
+++ programs/IdentityRunner.java
@@ -0,0 +1,171 @@
+class IdentityRunner{
+	public static void main(String[] names)
+	{
+		Identity.PrintNameAndEmail("Nayana G Y","nayanagy.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Anusha KM","anushakm.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Neeraj.p.Bhonsle","neeraj.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("veena.p","veenapagati.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Smruthi B S","smruthibs.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Shilpa U","shilpau.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Veena S","veenas.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Vinutha CR","vinuthacr.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Jeevika GS ","jeevikags.xworkz@gmail.com ");
+		Identity.PrintNameAndEmail("Rashmi K Kulkarni","rashmikrishna.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Sinchana Shindhe M.S.","sinchanashindhe.xworkz@gmail.com");
+		Identity.PrintNameAndEmail("Spoorthi G","spoorthignaikvs.xworkz@gmail.com");
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndMobile("Nayana G Y",9535230920l);
+		Identity.PrintNameAndMobile("Anusha KM Y",6362577433l);
+		Identity.PrintNameAndMobile("Neeraj.p.BhonsleY",8310198147l);
+		Identity.PrintNameAndMobile("Vijayalakshmi T G",7022454703l);
+		Identity.PrintNameAndMobile("SRISHAM S M",8277738399l);
+		Identity.PrintNameAndMobile("fazlam ",8050153518l);
+		Identity.PrintNameAndMobile("Hareesh Bewoor",6364535754l);
+		Identity.PrintNameAndMobile("Shubham Garve",8767938302l);
+		Identity.PrintNameAndMobile("Akshay  Y",9900238734l);
+		Identity.PrintNameAndMobile("Suraj S Y",7259303408l);
+		Identity.PrintNameAndMobile("Sandeep s sonnad ",9741426746l);
+		Identity.PrintNameAndMobile("Karthik. N",6362060352l);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndAge("Nayana G Y",22);
+		Identity.PrintNameAndAge("Anusha KM Y",22);
+		Identity.PrintNameAndAge("Neeraj.p.BhonsleY",22);
+		Identity.PrintNameAndAge("Vijayalakshmi T G",22);
+		Identity.PrintNameAndAge("SRISHAM S M",22);
+		Identity.PrintNameAndAge("fazlam ",22);
+		Identity.PrintNameAndAge("Hareesh Bewoor",22);
+		Identity.PrintNameAndAge("Shubham Garve",22);
+		Identity.PrintNameAndAge("Akshay  Y",22);
+		Identity.PrintNameAndAge("Suraj S Y",22);
+		Identity.PrintNameAndAge("Sandeep s sonnad ",22);
+		Identity.PrintNameAndAge("Karthik. N",22);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndAddress("Nayana G Y","nayanagy");
+		Identity.PrintNameAndAddress("Anusha KM","anushakm");
+		Identity.PrintNameAndAddress("Neeraj.p.Bhonsle","neeraj");
+		Identity.PrintNameAndAddress("veena.p","veenapagati");
+		Identity.PrintNameAndAddress("Smruthi B S","smruthibs");
+		Identity.PrintNameAndAddress("Shilpa U","shilpau");
+		Identity.PrintNameAndAddress("Veena S","veenas");
+		Identity.PrintNameAndAddress("Vinutha CR","vinuthacr");
+		Identity.PrintNameAndAddress("Jeevika GS ","jeevikags");
+		Identity.PrintNameAndAddress("Rashmi K Kulkarni","rashmikrishna");
+		Identity.PrintNameAndAddress("Sinchana Shindhe M.S.","sinchanashindhe");
+		Identity.PrintNameAndAddress("Spoorthi G","spoorthignaikvs");
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndEmailAndAddressAndAge("Nayana G Y","nayanagy.xworkz@gmail","nayanagy",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Anusha KM","anushakm.xworkz@gmail","anushakm",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Neeraj.p.Bhonsle","neeraj.xworkz@gmail","neeraj",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("veena.p","veenapagati.xworkz@gmail","veenapagati",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Smruthi B S","smruthibs.xworkz@gmail","smruthibs",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Shilpa U","shilpau.xworkz@gmail","shilpau",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Veena S","veenas.xworkz@gmail","veenas",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Vinutha CR","vinuthacr.xworkz@gmail","vinuthacr",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Jeevika GS ","jeevikags.xworkz@gmail","jeevikags",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Rashmi K Kulkarni","rashmikrishna.xworkz@gmail","rashmikrishna",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Sinchana Shindhe M.S.","sinchanashindhe.xworkz@gmail","sinchanashindhe",22);
+		Identity.PrintNameAndEmailAndAddressAndAge("Spoorthi G","spoorthignaikvs.xworkz@gmail","spoorthignaikvs",22);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndGender("Nayana G Y","Female");
+		Identity.PrintNameAndGender("Anusha KM","Female");
+		Identity.PrintNameAndGender("Neeraj.p.Bhonsle","Male");
+		Identity.PrintNameAndGender("veena.p","Female");
+		Identity.PrintNameAndGender("Smruthi B S","Female");
+		Identity.PrintNameAndGender("Shilpa U","Female");
+		Identity.PrintNameAndGender("Veena S","Female");
+		Identity.PrintNameAndGender("Vinutha CR","Female");
+		Identity.PrintNameAndGender("Jeevika GS ","Female");
+		Identity.PrintNameAndGender("Rashmi K Kulkarni","Female");
+		Identity.PrintNameAndGender("Sinchana Shindhe M.S.","Female");
+		Identity.PrintNameAndGender("Spoorthi G","Female");
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndWorking("Nayana G Y",true);
+		Identity.PrintNameAndWorking("Anusha KM",true);
+		Identity.PrintNameAndWorking("Neeraj.p.Bhonsle",true);
+		Identity.PrintNameAndWorking("veena.p",true);
+		Identity.PrintNameAndWorking("Smruthi B S",true);
+		Identity.PrintNameAndWorking("Shilpa U",true);
+		Identity.PrintNameAndWorking("Veena S",true);
+		Identity.PrintNameAndWorking("Vinutha CR",true);
+		Identity.PrintNameAndWorking("Jeevika GS ",true);
+		Identity.PrintNameAndWorking("Rashmi K Kulkarni",true);
+		Identity.PrintNameAndWorking("Sinchana Shindhe M.S.",true);
+		Identity.PrintNameAndWorking("Spoorthi G",true);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndShirtSize("Nayana G Y",22);
+		Identity.PrintNameAndShirtSize("Anusha KM Y",22);
+		Identity.PrintNameAndShirtSize("Neeraj.p.BhonsleY",22);
+		Identity.PrintNameAndShirtSize("Vijayalakshmi T G",22);
+		Identity.PrintNameAndShirtSize("SRISHAM S M",22);
+		Identity.PrintNameAndShirtSize("fazlam ",22);
+		Identity.PrintNameAndShirtSize("Hareesh Bewoor",22);
+		Identity.PrintNameAndShirtSize("Shubham Garve",22);
+		Identity.PrintNameAndShirtSize("Akshay  Y",22);
+		Identity.PrintNameAndShirtSize("Suraj S Y",22);
+		Identity.PrintNameAndShirtSize("Sandeep s sonnad ",22);
+		Identity.PrintNameAndShirtSize("Karthik. N",22);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndPantSize("Nayana G Y",22);
+		Identity.PrintNameAndPantSize("Anusha KM Y",22);
+		Identity.PrintNameAndPantSize("Neeraj.p.BhonsleY",22);
+		Identity.PrintNameAndPantSize("Vijayalakshmi T G",22);
+		Identity.PrintNameAndPantSize("SRISHAM S M",22);
+		Identity.PrintNameAndPantSize("fazlam ",22);
+		Identity.PrintNameAndPantSize("Hareesh Bewoor",22);
+		Identity.PrintNameAndPantSize("Shubham Garve",22);
+		Identity.PrintNameAndPantSize("Akshay  Y",22);
+		Identity.PrintNameAndPantSize("Suraj S Y",22);
+		Identity.PrintNameAndPantSize("Sandeep s sonnad ",22);
+		Identity.PrintNameAndPantSize("Karthik. N",22);
+		
+		System.out.println("");
+		System.out.println("");
+		
+		Identity.PrintNameAndWeightAndHeight("Nayana G Y",50,5);
+		Identity.PrintNameAndWeightAndHeight("Anusha KM Y",50,5);
+		Identity.PrintNameAndWeightAndHeight("Neeraj.p.BhonsleY",50,5);
+		Identity.PrintNameAndWeightAndHeight("Vijayalakshmi T G",50,5);
+		Identity.PrintNameAndWeightAndHeight("SRISHAM S M",50,5);
+		Identity.PrintNameAndWeightAndHeight("fazlam ",50,5);
+		Identity.PrintNameAndWeightAndHeight("Hareesh Bewoor",50,5);
+		Identity.PrintNameAndWeightAndHeight("Shubham Garve",50,5);
+		Identity.PrintNameAndWeightAndHeight("Akshay  Y",50,5);
+		Identity.PrintNameAndWeightAndHeight("Suraj S Y",50,5);
+		Identity.PrintNameAndWeightAndHeight("Sandeep s sonnad ",50,5);
+		Identity.PrintNameAndWeightAndHeight("Karthik. N",50,5);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
