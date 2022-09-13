54592070290b095e55948012afeb3cc6d173340e
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-13 10:22:31 +0530

    my second push

---
+++ programs/Forbyte.java
@@ -0,0 +1,44 @@
+class Forbyte{
+	public static void main(String[] arg){
+		byte a=-10;
+		byte b=6;
+		byte c=0;
+		byte d=2;
+		byte e=22;
+		byte f=1;
+		byte g=6;
+		byte h=8;
+		byte i=-8;
+		byte j=10;
+		System.out.println("a value is"+a);
+		System.out.println("b value is"+b);
+		System.out.println("c value is"+c);
+		System.out.println("d value is"+d);
+		System.out.println("e value is"+e);
+		System.out.println("f value is"+f);
+		System.out.println("g value is"+g);
+		System.out.println("h value is"+h);
+		System.out.println("i value is"+i);
+		System.out.println("j value is"+j);
+		 a=-100;
+		 b=60;
+		 c=02;
+		 d=20;
+		 e=2;
+		 f=10;
+		 g=60;
+		 h=80;
+		 i=-80;
+		 j=100;
+		System.out.println("a value is"+a);
+		System.out.println("b value is"+b);
+		System.out.println("c value is"+c);
+		System.out.println("d value is"+d);
+		System.out.println("e value is"+e);
+		System.out.println("f value is"+f);
+		System.out.println("g value is"+g);
+		System.out.println("h value is"+h);
+		System.out.println("i value is"+i);
+		System.out.println("j value is"+j);
+		}
+}
