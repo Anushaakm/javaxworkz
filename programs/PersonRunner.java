d8760853976ec168518c708abf4d08dfa7745715
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-18 22:02:02 +0530

    overloded

---
+++ programs/PersonRunner.java
@@ -0,0 +1,18 @@
+class PersonRunner{
+public static void main(String[] people){
+	Person.work("kyndrel",30000);
+	Person.work("kyndrel",30000," technical trainee");
+	Person.work("capgemini",35000);
+	Person.work("capgemini",35000,"web developer");
+	Person.work("Gyansysansys",30000);
+	Person.work("Gyansys",30000,"system engineer");
+	Person.work("IBM",450000);
+	Person.work("IBM",450000,"HR");
+	Person.work("Mind Tree",250000);
+	Person.work("Mind Tree",250000,"software engineer");
+	
+	
+	
+	
+}
+}
