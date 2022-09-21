807801cb6a045a15175ff609f1b52e846d3048bc
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-21 22:26:04 +0530

    sep21

---
+++ programs/ToothPaste.java
@@ -0,0 +1,169 @@
+class ToothPaste{
+	static String color;
+	static String odor;
+	static boolean conductivity;
+	static String state;
+	static String density;
+	static String buoyancy;
+	static String shape;
+	static double volume;
+	static int weight;
+	static float boilingPoint;
+	static float freezingPoint;
+	static float meltingPoint;
+	static boolean solubility;
+	static boolean hardness;
+	static boolean salt;
+	
+	static void setColor(String value)
+	{
+		color=value;
+	}
+	
+	static String getColor()
+	{
+		return color;
+	}
+	
+	static void setOdor(String value)
+	{
+		odor=value;
+	}
+	
+	static String getOdor()
+	{
+		return odor;
+	}
+	
+	static void isConductivity(boolean value)
+	{
+		conductivity=value;
+	}
+	
+	static boolean isConductivity()
+	{
+		return conductivity;
+	}
+	
+	static void setState(String value)
+	{
+		state=value;
+	}
+	
+	static String getState()
+	{
+		return state;
+	}
+	
+	static void setDensity(String value)
+	{
+		density=value;
+	}
+	
+	static String getDensity()
+	{
+		return density;
+	}
+	
+	static void setBuoyancy(String value)
+	{
+		buoyancy=value;
+	}
+	
+	static String getBuoyancy()
+	{
+		return buoyancy;
+	}
+	
+	static void setShape(String value)
+	{
+		shape=value;
+	}
+	
+	static String getShape()
+	{
+		return shape;
+	}
+	
+	static void setVolume(double value)
+	{
+		volume=value;
+	}
+	
+	static double getVolume()
+	{
+		return volume;
+	}
+	
+	static void setWeight(int value)
+	{
+		weight=value;
+	}
+	
+	static int getWeight()
+	{
+		return weight;
+	}
+	
+	static void setBoilingPoint(float value)
+	{
+		boilingPoint=value;
+	}
+	
+	static float getBoilingPoint()
+	{
+		return boilingPoint;
+	}
+	
+	static void setFreezingPoint(float value)
+	{
+		freezingPoint=value;
+	}
+	
+	static float getFreezingPoint()
+	{
+		return freezingPoint;
+	}
+	
+	static void setMeltingPoint(float value)
+	{
+		meltingPoint=value;
+	}
+	
+	static float getMeltingPoint()
+	{
+		return meltingPoint;
+	}
+	
+	static void isSolubility(boolean value)
+	{
+		solubility=value;
+	}
+	
+	static boolean isSolubility()
+	{
+		return solubility;
+	}
+	
+	static void isHardness(boolean value)
+	{
+		hardness=value;
+	}
+	
+	static boolean isHardness()
+	{
+		return hardness;
+	}
+	
+	static void isSalt(boolean value)
+	{
+		salt=value;
+	}
+	
+	static boolean isSalt()
+	{
+		return salt;
+	}
+	
+
+}
---
+++ programs/ToothPasteRunner.java
@@ -0,0 +1,58 @@
+class ToothPasteRunner
+{
+	public static void main(String[] args){
+		
+		System.out.println("ToothPasteRunner: naditha ide");
+		
+		ToothPaste.setColor("white");
+		System.out.println(ToothPaste.getColor());
+		
+		ToothPaste.setOdor("Mint");
+		System.out.println(ToothPaste.getOdor());
+		
+		ToothPaste.isConductivity(true);
+		System.out.println(ToothPaste.isConductivity());
+		
+		ToothPaste.setState("Paste");
+		System.out.println(ToothPaste.getState());
+		
+		ToothPaste.setDensity("A");
+		System.out.println(ToothPaste.getDensity());
+		
+		ToothPaste.setBuoyancy("B");
+		System.out.println(ToothPaste.getBuoyancy());
+		
+		ToothPaste.setShape("C");
+		System.out.println(ToothPaste.getShape());
+		
+		ToothPaste.setVolume(250D);
+		System.out.println(ToothPaste.getVolume());
+		
+		ToothPaste.setWeight(200);
+		System.out.println(ToothPaste.getWeight());
+		
+		ToothPaste.setBoilingPoint(10.0f);
+		System.out.println(ToothPaste.getBoilingPoint());
+		
+		ToothPaste.setFreezingPoint(20.0f);
+		System.out.println(ToothPaste.getFreezingPoint());
+		
+		ToothPaste.setMeltingPoint(30.0f);
+		System.out.println(ToothPaste.getMeltingPoint());
+		
+		ToothPaste.isSolubility(true);
+		System.out.println(ToothPaste.isSolubility());
+		
+		ToothPaste.isHardness(true);
+	System.out.println(ToothPaste.isHardness());
+		
+		ToothPaste.isSalt(true);
+		System.out.println(ToothPaste.isSalt());
+		
+		
+			System.out.println("ToothPasteRunner: mugithu");
+		
+	}
+
+
+}
