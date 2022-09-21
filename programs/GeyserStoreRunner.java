1ebb9748911a617286b486cade81c6bd6bc5e590
Anu_Harsha <anushakm.xworkz@gmail.com>
2022-09-21 23:01:37 +0530

    sep21

---
+++ programs/GeyserStoreRunner.java
@@ -0,0 +1,56 @@
+class GroceryStoreRunner
+{
+	public static void main(String[] args){
+		
+		System.out.println("GroceryStoreRunner: naditha ide");
+		
+		GroceryStore.setName("Shree Ram Grocery Store");
+		System.out.println(GroceryStore.getName());
+		
+		GroceryStore.setLocation("Rajajinagar");
+		System.out.println(GroceryStore.getLocation());
+		
+		GroceryStore.isOpen(true);
+		System.out.println(GroceryStore.isOpen());
+		
+		GroceryStore.setOwner("Ram");
+		System.out.println(GroceryStore.getOwner());
+		
+		GroceryStore.setNoWorkers(24);
+		System.out.println(GroceryStore.getNoWorkers());
+		
+		GroceryStore.isOnline(true);
+		System.out.println(GroceryStore.isOnline());
+		
+		GroceryStore.isWifi(true);
+		System.out.println(GroceryStore.isWifi());
+		
+		GroceryStore.setPaymentMethod("cash on");
+		System.out.println(GroceryStore.getPaymentMethod());
+		
+		GroceryStore.setWeight(200);
+		System.out.println(GroceryStore.getWeight());
+		
+		GroceryStore.isExchange(true);
+		System.out.println(GroceryStore.isExchange());
+		
+		GroceryStore.setCountry("India");
+		System.out.println(GroceryStore.getCountry());
+		
+		GroceryStore.setState("karnataka");
+		System.out.println(GroceryStore.getState());
+		
+		GroceryStore.setSize("L");
+		System.out.println(GroceryStore.getSize());
+		
+		GroceryStore.setStaffSalary(10000);
+		System.out.println(GroceryStore.getStaffSalary());
+		
+		
+		
+			System.out.println("ToothPasteRunner: mugithu");
+		
+	}
+
+
+}
---
+++ programs/GroceryStore.java
@@ -0,0 +1,161 @@
+class GroceryStore{
+	static String name;
+	static String location;
+	static boolean open;
+	static String owner;
+	static int noWorkers;
+	static boolean online;
+	static boolean wifi;
+	static String paymentMethod;
+	static boolean exchange;
+	static String country;
+	static String state;
+	static long contactno;
+	static String size;
+	static int staffSalary;
+	
+	static void setName(String value)
+	{
+		name=value;
+	}
+	
+	static String getName()
+	{
+		return name;
+	}
+	
+	static void setLocation(String value)
+	{
+		location=value;
+	}
+	
+	static String getLocation()
+	{
+		return location;
+	}
+	
+	static void isOpen(boolean value)
+	{
+		open=value;
+	}
+	
+	static boolean isOpen()
+	{
+		return open;
+	}
+	
+	static void setOwner(String value)
+	{
+		owner=value;
+	}
+	
+	static String getOwner()
+	{
+		return owner;
+	}
+	
+	static void setNoWorkers(int value)
+	{
+		noWorkers=value;
+	}
+	
+	static int getNoWorkers()
+	{
+		return noWorkers;
+	}
+	
+	static void isOnline(boolean value)
+	{
+		online=value;
+	}
+	
+	static boolean isOnline()
+	{
+		return online;
+	}
+	
+	static void isWifi(boolean value)
+	{
+		wifi=value;
+	}
+	
+	static boolean isWifi()
+	{
+		return wifi;
+	}
+	
+	static void setPaymentMethod(String value)
+	{
+		paymentMethod=value;
+	}
+	
+	static String getPaymentMethod()
+	{
+		return paymentMethod;
+	}
+	
+	static void isExchange(boolean value)
+	{
+		exchange=value;
+	}
+	
+	static boolean isExchange()
+	{
+		return exchange;
+	}
+	
+	static void setCountry(String value)
+	{
+		country=value;
+	}
+	
+	static String getCountry()
+	{
+		return country;
+	}
+	
+	static void setState(String value)
+	{
+		state=value;
+	}
+	
+	static String getState()
+	{
+		return state;
+	}
+	
+	static void setContact(long value)
+	{
+		contactno=value;
+	}
+	
+	static long getContact()
+	{
+		return contactno;
+	}
+	
+	static void setSize(String value)
+	{
+		size=value;
+	}
+	
+	static String getSize()
+	{
+		return size;
+	}
+	
+	static void setStaffSalary(int value)
+	{
+		staffSalary=value;
+	}
+	
+	static int getStaffSalary()
+	{
+		return staffSalary;
+	}
+
+	
+	
+	
+}
+
